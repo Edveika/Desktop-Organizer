55ce115609f8bd164ab6d6bb933b13b68e7b3696
Edveika <edvinasbureika@gmail.com>
2024-03-31 15:56:37 +0300

    Detect os, select/create cache dir

--- Desktop-Organizer/SettingsCache.py
+++ Desktop-Organizer/SettingsCache.py
@@ -1,7 +1,18 @@
 from Setting import Setting
+import platform
+import dirs
+import os
 
 class SettingsCache:
   def __init__(self) -> None:
-    pass
+    os_name = platform.system()
 
-  
+    match os_name:
+      case "Windows": self.cache_path = dirs.WINDOWS_SETTINGS_DIR
+      case "Linux": self.cache_path = dirs.LINUX_SETTINGS_DIR
+      case "Darwin": self.cache_path = dirs.MACOS_SETTINGS_DIR
+      case _:
+        raise Exception("Unsupported Operating System")
+
+    if not os.path.exists("self.cache_path"):
+      os.makedirs(self.cache_path)
