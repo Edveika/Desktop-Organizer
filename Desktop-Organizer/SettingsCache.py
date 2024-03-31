8586f9b8bf3fb31b31965df99bce617e48d0b8cb
Edveika <edvinasbureika@gmail.com>
2024-03-31 15:50:35 +0300

    Directories for linux, mac and windows

--- Desktop-Organizer/SettingsCache.py
+++ Desktop-Organizer/SettingsCache.py
@@ -1,3 +1,7 @@
+from Setting import Setting
+
 class SettingsCache:
   def __init__(self) -> None:
     pass
+
+  
---
+++ Desktop-Organizer/dirs.py
@@ -0,0 +1,5 @@
+import os
+
+LINUX_SETTINGS_DIR = os.path.expanduser("~/.config/DesktopOrganizer/")
+MACOS_SETTINGS_DIR = os.path.expanduser("~/Library/Preferences/DesktopOrganizer/")
+WINDOWS_SETTINGS_DIR = os.path.expandvars("C:\\Users\\%USERNAME%\\AppData\\Local\\DesktopOrganizer\\")
